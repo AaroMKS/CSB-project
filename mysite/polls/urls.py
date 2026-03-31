0781b64f94032459cce28c5f263f00b061b70f81
Aaro Savolainen <aaro.savolainen@helsinki.fi>
2026-03-31 22:25:21 +0300

    Created the site with 4 flaws

---
+++ mysite/polls/urls.py
@@ -0,0 +1,15 @@
+from django.urls import path
+
+
+from . import views
+
+app_name = 'polls'
+urlpatterns = [
+    path('', views.IndexView.as_view(), name='index'),
+    path('<int:pk>/', views.DetailView.as_view(), name='detail'),
+    path('<int:pk>/results/', views.ResultsView.as_view(), name='results'),
+    path('<int:question_id>/vote/', views.vote, name='vote'),
+    path('<int:question_id>/comment/', views.add_comment, name='add_comment'),
+    path('search/', views.search_questions, name='search'),
+    path('delete/<int:comment_id>/', views.delete_comment, name='delete_comment'),
+]
---
+++ mysite/polls/views.py
@@ -0,0 +1,75 @@
+from django.shortcuts import get_object_or_404, render
+from django.http import HttpResponse, HttpResponseRedirect
+from django.views import generic
+from django.urls import reverse
+from django.contrib.auth.decorators import login_required
+
+from .models import Choice, Question, Comment
+
+
+class IndexView(generic.ListView):
+    template_name = 'polls/index.html'
+    context_object_name = 'latest_question_list'
+
+    def get_queryset(self):
+        """Return the last five published questions."""
+        return Question.objects.order_by('-pub_date')[:5]
+
+
+class DetailView(generic.DetailView):
+    model = Question
+    template_name = 'polls/detail.html'
+
+
+class ResultsView(generic.DetailView):
+    model = Question
+    template_name = 'polls/results.html'
+
+def vote(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    try:
+        selected_choice = question.choice_set.get(pk=request.POST['choice'])
+    except (KeyError, Choice.DoesNotExist):
+        # Redisplay the question voting form.
+        return render(request, 'polls/detail.html', {
+            'question': question,
+            'error_message': "You didn't select a choice.",
+        })
+    else:
+        selected_choice.votes += 1
+        selected_choice.save()
+        # Always return an HttpResponseRedirect after successfully dealing
+        # with POST data. This prevents data from being posted twice if a
+        # user hits the Back button.
+        return HttpResponseRedirect(reverse('polls:results', args=(question.id,)))
+@login_required
+def add_comment(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    
+    if request.method == "POST":
+        text = request.POST.get("text")
+        user= request.user
+        Comment.objects.create(question=question, text=text, user=request.user)
+        return HttpResponseRedirect(reverse('polls:detail', args=(question.id,)))
+
+def search_questions(request):
+    query = request.GET.get('q', '')
+
+    sql = f"SELECT * FROM polls_question WHERE question_text LIKE '%{query}%'"
+    questions = Question.objects.raw(sql)
+    #Fix:
+    # sql = "SELECT * FROM polls_question WHERE question_text LIKE %s"
+    # questions = Question.objects.raw(sql, [f"%{query}%"]
+    return HttpResponse(questions)
+
+@login_required
+def delete_comment(request, comment_id):
+    comment = Comment.objects.get(id=comment_id)
+    #Fix
+    # if comment.user==request.user:
+    #     comment.delete()
+    # else:
+    #     return HttpResponse("Forbidden". status=403)
+    comment.delete()
+    return HttpResponse("Deleted")
+# Create your views here.
